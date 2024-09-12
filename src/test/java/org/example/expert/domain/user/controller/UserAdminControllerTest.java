273d04d4f80d6b3763b33bbca9e074673e8df65f
wjdtmdgjs-1 <jeong5207@naver.com>
2024-09-13 07:38:55 +0900

    userController userAdminTest complete except 1

--- src/test/java/org/example/expert/domain/user/controller/UserAdminControllerTest.java
+++ src/test/java/org/example/expert/domain/user/controller/UserAdminControllerTest.java
@@ -1,4 +1,40 @@
 package org.example.expert.domain.user.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.expert.domain.user.dto.request.UserRoleChangeRequest;
+import org.example.expert.domain.user.service.UserAdminService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserAdminController.class)
 public class UserAdminControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private UserAdminService userAdminService;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void changeUserRole테스트() throws Exception {
+        long userId = 1L;
+        UserRoleChangeRequest userRoleChangeRequest = new UserRoleChangeRequest("ADMIN");
+
+        doNothing().when(userAdminService).changeUserRole(userId, userRoleChangeRequest);
+
+        ResultActions resultActions = mockMvc.perform(patch("/admin/users/{userId}", userId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userRoleChangeRequest)));
+
+        resultActions.andExpect(status().isOk());
+    }
 }
--- src/test/java/org/example/expert/domain/user/controller/UserControllerTest.java
+++ src/test/java/org/example/expert/domain/user/controller/UserControllerTest.java
@@ -2,6 +2,7 @@ package org.example.expert.domain.user.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
 import org.example.expert.config.AuthUserArgumentResolver;
 import org.example.expert.config.GlobalExceptionHandler;
 import org.example.expert.domain.common.dto.AuthUser;
@@ -15,7 +16,9 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ClientHttpRequest;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -65,13 +68,15 @@ public class UserControllerTest {
     public void changePassword테스트() throws Exception {
         AuthUser authUser = TEST_AUTHUSER;
 
+        *//*given(resolver.resolveArgument(any(), any(), any(), any()))
+                .willReturn(authUser);*//*
+
         UserChangePasswordRequest userChangePasswordRequest =
                 new UserChangePasswordRequest("1234","1234");
 
         doNothing().when(userService).changePassword(anyLong(), any());
 
         ResultActions resultActions = mockMvc.perform(put("/users")
-                        .
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(userChangePasswordRequest)));
 
