4450f773eb43feaf48ea27def7a79a0c684fedfd
wjdtmdgjs-1 <jeong5207@naver.com>
2024-09-12 22:36:40 +0900

    userAdminServiceTest complete

---
+++ src/test/java/org/example/expert/domain/user/service/UserAdminServiceTest.java
@@ -0,0 +1,56 @@
+package org.example.expert.domain.user.service;
+
+import org.example.expert.domain.common.exception.InvalidRequestException;
+import org.example.expert.domain.user.dto.request.UserRoleChangeRequest;
+import org.example.expert.domain.user.entity.User;
+import org.example.expert.domain.user.enums.UserRole;
+import org.example.expert.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Optional;
+
+import static org.example.expert.domain.CommonNeeds.TEST_USER1;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class UserAdminServiceTest {
+    @Mock
+    UserRepository userRepository;
+    @InjectMocks
+    UserAdminService userAdminService;
+
+    @Test
+    void changeUserRole실패_유저찾기실패(){
+        long userId =1;
+        given(userRepository.findById(userId)).willReturn(Optional.empty());
+
+        InvalidRequestException exception = assertThrows(InvalidRequestException.class,()->{
+            userAdminService.changeUserRole(userId,new UserRoleChangeRequest());
+        });
+
+        assertEquals("User not found",exception.getMessage());
+    }
+
+    @Test
+    void changeUserRole_작동성공테스트(){
+        User user = TEST_USER1;
+        UserRoleChangeRequest userRoleChangeRequest = new UserRoleChangeRequest("ADMIN");
+        ReflectionTestUtils.setField(user,"id",1L);
+        long userId =1;
+        given(userRepository.findById(userId)).willReturn(Optional.of(user));
+
+        //when
+        userAdminService.changeUserRole(userId,userRoleChangeRequest);
+        //then
+
+        assertEquals(UserRole.of(userRoleChangeRequest.getRole()),user.getUserRole());
+    }
+}
